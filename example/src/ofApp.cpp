b85b69d2af4dcd456a4d5b569a493a1f7c94b3f6
Eric Mika <eric@ericmika.com>
2015-11-08 23:15:32 -0500

    Working on OF 9 / OS X 10.11 / 64... including pango.

--- example/src/ofApp.cpp
+++ example/src/ofApp.cpp
@@ -1,90 +1,92 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
-    ofSetRectMode(OF_RECTMODE_CORNER);
-    
-    renderer.setSize(ofGetWidth(), ofGetHeight());
-    renderer.setupColorPicker(ofGetWidth(), ofGetHeight(), 2, 3);
-    renderer.startTuio(3333);
+void ofApp::setup() {
+  ofSetRectMode(OF_RECTMODE_CORNER);
 
-    cont.enableDepthTest();
-    for(int i=0;i<40;i++){
-        buttons[i].setSize(100, 100);
-        buttons[i].setPosition(ofRandomWidth(),ofRandomHeight());
-        buttons[i].setColor(255, 0, 0);
-        buttons[i].enableDepthTest();
-        buttons[i].setRandomColor();
-        cont.addChild(&buttons[i]);
-    }
-    renderer.addChild(&cont);
- 
-//    ofSetVerticalSync(false);
-}
+  renderer.setSize(ofGetWidth(), ofGetHeight());
+  renderer.setupColorPicker(ofGetWidth(), ofGetHeight(), 2, 3);
+  renderer.startTuio(3333);
 
+  cont.enableDepthTest();
+  for (int i = 0; i < 40; i++) {
+    buttons[i].setSize(100, 100);
+    buttons[i].setPosition(ofRandomWidth(), ofRandomHeight());
+    buttons[i].setColor(255, 0, 0);
+    buttons[i].enableDepthTest();
+    buttons[i].setRandomColor();
+    // cont.addChild(&buttons[i]);
+  }
 
-//--------------------------------------------------------------
-void ofApp::update(){
-    ofSetWindowTitle(ofToString(ofGetFrameRate()));
-    
-    
-}
+  textField.setSize(500, 500);
+  textField.setText("TEST! <b>how bold!</b> <i>such emphasis!</i>");
 
-//--------------------------------------------------------------
-void ofApp::draw(){
-    ofBackground(0,0,0);
-    renderer.draw();
+  cont.addChild(&textField);
+
+  renderer.addChild(&cont);
+
+  //    ofSetVerticalSync(false);
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-    if(key=='f'){
-        ofToggleFullscreen();
-    }
-    if(key=='c'){
-        ofCamera* camera=renderer.getCamera();
-        camera->rotate(30 , 0, 1, 0);
-        
-    }
+void ofApp::update() {
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
+void ofApp::draw() {
+  ofBackground(0, 0, 0);
+  renderer.draw();
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y){
-
+void ofApp::keyPressed(int key) {
+  if (key == 'f') {
+    ofToggleFullscreen();
+  }
+  if (key == 'c') {
+    ofCamera *camera = renderer.getCamera();
+    camera->rotate(30, 0, 1, 0);
+  }
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
+void ofApp::keyReleased(int key) {
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
+void ofApp::mouseMoved(int x, int y) {
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
+void ofApp::mouseDragged(int x, int y, int button) {
 }
 
 //--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-    renderer.resize();
-    renderer.setupColorPicker(ofGetWidth(), ofGetHeight(), 2, 3);
+void ofApp::mousePressed(int x, int y, int button) {
 }
 
 //--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
+void ofApp::mouseReleased(int x, int y, int button) {
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::mouseEntered(int x, int y) {
+}
 
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y) {
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {
+  renderer.resize();
+  renderer.setupColorPicker(ofGetWidth(), ofGetHeight(), 2, 3);
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg) {
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) {
 }
